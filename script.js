2d17707b516f06ee7b14ed8c4cd6d845a032518e
dontMakeMeQQ <32247143+dontMakeMeQQ@users.noreply.github.com>
2017-10-18 18:53:03 +0200

    poprawki

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ function myFunction() {
     var text = document.getElementById("text");
     var li = document.createElement("li");
     
-    if (document.getElementById("text").value == '') {
-        document.getElementById('QQQ').removeAttribute('hidden');
+    if (text.value.trim() == "") {
+        validation.removeAttribute('hidden');
 
-    } else {
+    }
+    else {
         li.setAttribute('id', text.value);
         li.appendChild(document.createTextNode(text.value));
         ul.appendChild(li);
-        document.getElementById('text').value = "";
-
-        document.getElementById('QQQ').setAttribute('hidden', true);
+        text.value = "";
+        validation.setAttribute('hidden', true);
     }
 
 }
