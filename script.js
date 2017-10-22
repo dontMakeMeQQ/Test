bea7890874f5aea676f0fca01086f1f64fcd819c
dontMakeMeQQ <32247143+dontMakeMeQQ@users.noreply.github.com>
2017-10-22 15:45:10 +0200

    task2

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-function myFunction() {
-    var ul = document.getElementById("list");
-    var text = document.getElementById("text");
-    var li = document.createElement("li");
+
+button.setAttribute("removeFunction")
+    function myFunction() {
+        var ul = document.getElementById("list");
+        var text = document.getElementById("text");
+        var li = document.createElement("li");
+        //Remove li button
+        var button = document.createElement("button")
+        button.innerHTML = "Usuń"
+        button.onclick = function(){
+            var conf = confirm('Czy na pewno chcesz usunąć to zadanie?')
+            if (conf == true){
+                this.parentNode.parentNode.removeChild(this.parentNode)
+            }
+            
+
+            
+ 
+        }
+
+        if (text.value.trim() == "") {
+            validation.removeAttribute('hidden');
     
-    if (document.getElementById("text").value == '') {
-        document.getElementById('QQQ').removeAttribute('hidden');
-
-    } else {
-        li.setAttribute('id', text.value);
-        li.appendChild(document.createTextNode(text.value));
-        ul.appendChild(li);
-        document.getElementById('text').value = "";
-
-        document.getElementById('QQQ').setAttribute('hidden', true);
+        }
+        else {
+            li.appendChild(button);
+            li.setAttribute('id', text.value);
+            li.appendChild(document.createTextNode(text.value));
+            ul.appendChild(li);
+            text.value = "";
+            validation.setAttribute('hidden', true);
+        }
     }
-
-}
