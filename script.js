9a873116c50d48ae2e0f3eb2f58025b1f8e59e0a
dontMakeMeQQ <32247143+dontMakeMeQQ@users.noreply.github.com>
2017-11-09 21:43:20 +0100

    working backend

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
-button.setAttribute("removeFunction")
-    function myFunction() {
-        var ul = document.getElementById("list");
-        var text = document.getElementById("text");
-        var li = document.createElement("li");
-        //Remove li button
-        var button = document.createElement("button")
-        button.innerHTML = "Usuń"
-        button.onclick = function(){
-            var conf = confirm('Czy na pewno chcesz usunąć to zadanie?')
-            if (conf == true){
-                this.parentNode.parentNode.removeChild(this.parentNode)
-            }
-            
 
-            
- 
-        }
-
-        if (text.value.trim() == "") {
-            validation.removeAttribute('hidden');
-    
-        }
-        else {
-            li.appendChild(button);
-            li.setAttribute('id', text.value);
-            li.appendChild(document.createTextNode(text.value));
-            ul.appendChild(li);
-            text.value = "";
-            validation.setAttribute('hidden', true);
-        }
+function loadAllTasks(){
+    var request = new XMLHttpRequest
+    request.onreadystatechange = function(){
+        if(request.readyState == 4 && request.status == 200)
+        addAllTasksToHTML(JSON.parse(request.responseText))
     }
+    request.open("GET", "http://localhost:8080/Tasks", true)
+    request.send(null)
+}
+function addAllTasksToHTML(tasks){
+   tasks.forEach(addOneTaskToHTML)
+}
+function addOneTaskToHTML(task){
+  var tasksList = document.getElementById('list')
+  var newTask = document.createElement('li')
+  newTask.appendChild(document.createTextNode(task.Name))
+  tasksList.appendChild(newTask)
+}    
+function addNewTask(){
+    var newTaskName = document.getElementById('text').value;
+	addNewTaskOnServer(newTaskName)
+}
 
+function addNewTaskOnServer(taskName){
+	var request = new XMLHttpRequest();
+	request.onreadystatechange = function(){
+		if (request.readyState == 4 && request.status == 200)
+			addOneTaskToHTML({Name:taskName})
+	}
+	request.open("POST", "http://localhost:8080/Tasks", true); // true for asynchronous
+	var newTask = {TaskName:taskName};
+    request.send(JSON.stringify(newTask))
+}
